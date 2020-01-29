04deb73058dabc9f70cc6e9b4984eb85a8004eb6
Cristian Franco <cristian.franco@docdigitales.com>
2020-01-28 16:24:28 -0800

    se crea controlador User

---
+++ db/migrate/20200129002223_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password
+      t.string :full_name
+      t.string :rfc
+      t.string :company_name
+
+      t.timestamps
+    end
+  end
+end
