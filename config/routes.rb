7c786a52cb6cbdfcdc1c644452677b07c107a815
Cristian Franco <cristian.franco@docdigitales.com>
2020-01-29 16:36:18 -0800

    loggin recuperado

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :sucursals
-  resources :empleados 
+  resources :empleados
+  root 'sessions#new' 
   get 'home', to: 'home#index', as: 'home'
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
